b53811b1e7cd98f539146bf6607d42da9b78c873
Jonathan Herman <jdh11235@gmail.com>
2017-11-07 15:59:44 -0600

    make the clock svg square

--- app.js
+++ app.js
@@ -1,13 +1,9 @@
 var clock = d3.select('.clock')
-  .selectAll('circle')
-  .data([1])
 
-clock.enter().append('circle')
+clock.append('circle')
   .style('fill', 'transparent')
   .style('stroke', 'black')
-  .style('stroke-width', '1vmin')
-  .style('cx', '50vw')
-  .style('cy', '50vh')
-  .style('r', '40vmin')
-
-clock.exit().remove()
+  .style('stroke-width', '10vmin')
+  .style('cx', '50vmin')
+  .style('cy', '50vmin')
+  .style('r', '37vmin')
