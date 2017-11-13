af45846c9496a399711082b7f9179e5ae215ce95
Jonathan Herman <jdh11235@gmail.com>
2017-11-13 16:37:22 -0600

    hand scale

--- app.js
+++ app.js
@@ -13,13 +13,13 @@ let quarterHourScale = d3.scaleLinear()
 face.selectAll('.quarter-hour-tick')
   .data(Array(96))
   .enter()
-    .append('line')
-      .classed('quarter-hour-tick', true)
-      .attr('x1', '0')
-      .attr('x2', '0')
-      .attr('y1', '42vmin')
-      .attr('y2', '38vmin')
-      .attr('transform', (d, i) => `rotate(${quarterHourScale(i)})`)
+  .append('line')
+  .classed('quarter-hour-tick', true)
+  .attr('x1', '0')
+  .attr('x2', '0')
+  .attr('y1', '42vmin')
+  .attr('y2', '38vmin')
+  .attr('transform', (d, i) => `rotate(${quarterHourScale(i)})`)
 
 let hourScale = d3.scaleLinear()
   .range([0, 360 - 360 / 24])
@@ -28,19 +28,66 @@ let hourScale = d3.scaleLinear()
 face.selectAll('.hour-tick')
   .data(Array(24))
   .enter()
-    .append('line')
-      .classed('hour-tick', true)
-      .attr('x1', '0')
-      .attr('x2', '0')
-      .attr('y1', '42vmin')
-      .attr('y2', '38vmin')
-      .attr('transform', (d, i) => `rotate(${hourScale(i)})`)
+  .append('line')
+  .classed('hour-tick', true)
+  .attr('x1', '0')
+  .attr('x2', '0')
+  .attr('y1', '42vmin')
+  .attr('y2', '38vmin')
+  .attr('transform', (d, i) => `rotate(${hourScale(i)})`)
 
 face.selectAll('.hour-text')
-  .data(['00', '01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23'])
+  .data([
+    '00',
+    '01',
+    '02',
+    '03',
+    '04',
+    '05',
+    '06',
+    '07',
+    '08',
+    '09',
+    '10',
+    '11',
+    '12',
+    '13',
+    '14',
+    '15',
+    '16',
+    '17',
+    '18',
+    '19',
+    '20',
+    '21',
+    '22',
+    '23'
+  ])
   .enter()
-    .append('text')
-      .classed('hour-text', true)
-      .text(d => d)
-      .attr('x', (d, i) => `${33 * Math.sin(Math.PI / 180 * (hourScale(i) + 180))}vmin`)
-      .attr('y', (d, i) => `${-33 * Math.cos(Math.PI / 180 * (hourScale(i) + 180))}vmin`)
+  .append('text')
+  .classed('hour-text', true)
+  .text(d => d)
+  .attr('x', (d, i) => `${33 * Math.sin(Math.PI / 180 * (hourScale(i) + 180))}vmin`)
+  .attr('y', (d, i) => `${-33 * Math.cos(Math.PI / 180 * (hourScale(i) + 180))}vmin`)
+
+let handScale = d3.scaleLinear()
+  .range([0, 360 - 360 / (24 * 60)])
+  .domain([0, (24 * 60) - 1])
+
+face.selectAll('.hand')
+  .data([
+    {zone: 'America/New_York', label: 'New York'},
+    {zone: 'America/Chicago', label: 'Chicago'},
+    {zone: 'America/Denver', label: 'Denver'},
+    {zone: 'America/Los_Angeles', label: 'San Francisco'}
+  ])
+  .enter()
+  .append('line')
+  .classed('hand', true)
+  .attr('x1', '0')
+  .attr('x2', '0')
+  .attr('y1', '40.3vmin')
+  .attr('y2', '48vmin')
+  .attr('transform', d => `rotate(${
+    handScale(moment().tz(d.zone).diff(moment().tz(d.zone).startOf('day'), 'minutes'))
+  })`)
